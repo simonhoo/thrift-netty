792526ccee798fcb4df16ec60a6e3d91c07bf69d
Simon.Hoo <Info@cottsoft.com>
2014-12-14 11:10:52 +0800

    提交Thrift on Netty代码框架。

---
+++ thrift-netty/thrift-netty-example/src/main/java/com/cottsoft/thrift/netty/example/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2005-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Author:	Simon.Hoo
+ * Blog:   	http://www.cottsoft.com 
+ * Email:	Info@cottsoft.com
+ * 
+ * You can @simonhoo  on Github.com, weibo.com, twitter, t.qq.com
+ */
+
+/**
+ * @author  Simon.Hoo(Info@cottsoft.com)
+ * @version v1.0.0
+ */
+
+package com.cottsoft.thrift.netty.example;
